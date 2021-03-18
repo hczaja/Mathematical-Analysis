e552c664514dd44c5c55b7bb7baf3714370918f5
Hubert Czaja <hczaja@prescientco.com>
2021-03-18 12:08:11 +0100

    ES-1 Typo fix

--- MathematicalAnalysis/MathematicalAnalysis.cpp
+++ MathematicalAnalysis/MathematicalAnalysis.cpp
@@ -10,6 +10,6 @@ int main()
 
 	std::cout << "a1 belongs to A: " << BelongsTo(a1, A) << std::endl;
 	std::cout << "a2 belongs to A: " << BelongsTo(a2, A) << std::endl;
-	std::cout << "B is contained in A: " << IsContainedIn(A, B) << std::endl;
-	std::cout << "A is contained in B: " << IsContainedIn(B, A) << std::endl;
+	std::cout << "B is contained in A: " << IsContainedIn(B, A) << std::endl;
+	std::cout << "A is contained in B: " << IsContainedIn(A, B) << std::endl;
 }
