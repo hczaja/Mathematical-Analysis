14a32544f295fa2d3d5036bce1045461f6bfc77f
Hubert Czaja <hczaja@prescientco.com>
2021-03-18 11:22:37 +0100

    ES-1 Introduction to analysis

--- MathematicalAnalysis/MathematicalAnalysis.cpp
+++ MathematicalAnalysis/MathematicalAnalysis.cpp
@@ -1,20 +1,56 @@
-// MathematicalAnalysis.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <algorithm>>
+#include <assert.h>
+
+void BelongsTo();
+void ContainedIn();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	BelongsTo();
+	ContainedIn();
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void BelongsTo()
+{
+	std::vector<int> A = { 1, 2, 3 };
+	int a1 = 2, a2 = 4;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	struct Contains
+	{
+		const int a;
+		Contains(int _a) : a(_a) {}
+
+		bool operator()(int _a) const { return this->a == _a; }
+	};
+
+	bool a1BelongsToA = std::any_of(A.cbegin(), A.cend(), Contains(a1));
+	bool a2BelongsToA = std::any_of(A.cbegin(), A.cend(), Contains(a2));
+
+	assert(a1BelongsToA);
+	assert(!a2BelongsToA);
+}
+
+void ContainedIn()
+{
+	std::vector<int> A = { 1, 2, 3 };
+	std::vector<int> B = { 1, 2 };
+
+	struct Contains
+	{
+		const int a;
+		Contains(int _a) : a(_a) {}
+
+		bool operator()(int _a) const { return this->a == _a; }
+	};
+
+	bool BisContainedInA = std::all_of(B.cbegin(), B.cend(), [A](int b) {
+		return std::any_of(A.cbegin(), A.cend(), Contains(b)); });
+
+	bool AisContainedInB = std::all_of(A.cbegin(), A.cend(), [B](int a) {
+		return std::any_of(B.cbegin(), B.cend(), Contains(a)); });
+
+	assert(BisContainedInA);
+	assert(!AisContainedInB);
+}
